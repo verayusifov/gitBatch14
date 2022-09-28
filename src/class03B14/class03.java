06b916f86ec28e15aead2297ec454e283707b723
verayusifov <verayusif80@gmail.com>
2022-09-28 14:43:15 -0400

    simple basic project

---
+++ src/class03B14/class03.java
@@ -0,0 +1,11 @@
+package class03B14;
+
+public class class03 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hey, this is a git class");
+
+
+	}
+
+}
